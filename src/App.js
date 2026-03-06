92a1ba6cd7a1c1b2cf8b3a45a064adc9b3c8900d
Diego Mesa <darman1987@hotmail.com>
2026-03-06 13:54:08 -0800

    adding sticky menu

--- src/App.js
+++ src/App.js
@@ -12,18 +12,20 @@ const App = () => {
     return (
         <AppProvider>
             <div className='container app-shell py-4'>
-                <div className='d-flex justify-content-between align-items-center app-header'>
-                    <h1 className='mb-0'>Budget Allocation</h1>
-                </div>
-                <div className='row g-3 mt-2'>
-                    <div className='col-12 col-md-6 col-xl-4'>
-                        <Budget />
+                <div className='summary-sticky'>
+                    <div className='d-flex justify-content-between align-items-center app-header'>
+                        <h1 className='mb-0'>Budget Allocation</h1>
                     </div>
-                    <div className='col-12 col-md-6 col-xl-4'>
-                        <RemainingBudget />
-                    </div>
-                    <div className='col-12 col-md-6 col-xl-4'>
-                        <ExpenseTotal />
+                    <div className='row g-3 mt-2'>
+                        <div className='col-12 col-md-6 col-xl-4'>
+                            <Budget />
+                        </div>
+                        <div className='col-12 col-md-6 col-xl-4'>
+                            <RemainingBudget />
+                        </div>
+                        <div className='col-12 col-md-6 col-xl-4'>
+                            <ExpenseTotal />
+                        </div>
                     </div>
                 </div>
                 <div className='row mt-3'>
